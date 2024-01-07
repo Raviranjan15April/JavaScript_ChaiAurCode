e248df2b884acaf6f140f2233a9fd378f69f976d
Raviranjan15April <146761397+Raviranjan15April@users.noreply.github.com>
2024-01-07 13:02:37 +0000

    Data Type summary

---
+++ JavaScript_Basic/05_DatatypeSummary.js
@@ -0,0 +1,20 @@
+/*
+Summary of Datatype:-
+primitive data type:- number, string, boolean, undefined, null, Bigint & Symbol
+non primitive(reference) data type:- Array, object, function
+Note:- typeof null is object, typeof undefined is undefined and type of function is function.
+*/
+// Symbol are used for uniqueness.
+let sym01 = Symbol('123');
+let sym02 = Symbol('123');
+console.log(sym01 == sym02);        // false
+console.log(sym01 === sym02);       // false
+
+// BigInt are used for big number.
+let bigNumber = BigInt(1234);
+console.log(bigNumber);             // 1234n
+console.log(typeof bigNumber);      // bigint
+
+let bigNumber02 = 1234n;
+console.log(bigNumber02);           // 1234n
+console.log(typeof bigNumber02);    // bigint
