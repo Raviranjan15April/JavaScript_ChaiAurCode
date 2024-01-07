f6df188af091d9e86ce7306139a296db360861d0
Raviranjan15April <146761397+Raviranjan15April@users.noreply.github.com>
2024-01-07 08:50:19 +0000

    variables in JavaScript

---
+++ JavaScript_Basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 12345;
+let accountName = "John Deo";
+var accountEmail = "john.deo@gmail.com";
+var accountPassword = "John@deo123";
+state = "Bengaluru";
+
+console.table([accountId, accountName,accountEmail, accountPassword, state]);
+
+/*
+variable can be created by using "var", "let" & "const" keywords.
+variable created by "const" will not change. that's why it is constant.
+variable created by "var" & "let" can be change in future.
+Note:- we can create variable without giving any keywords, but avoid to create variable like this.
+
+Note:- avoid to create variable by "var" keyword becouse there is limitation as block scope.
+*/
