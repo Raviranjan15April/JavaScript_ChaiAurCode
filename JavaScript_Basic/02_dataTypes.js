d61bdd8281775c6961d2a00f65390eb3bbba26eb
Raviranjan15April <146761397+Raviranjan15April@users.noreply.github.com>
2024-01-07 09:35:40 +0000

    Data Types in Javascript

---
+++ JavaScript_Basic/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";       // Treat all javascript code as new version.
+
+/*
+Datatypes in javaScript:-
+a. Primitive Data types:-
+    number --> numeric value
+    string --> can we used " "/''.
+    boolean --> true/false
+    BigInt --> used largest value 
+    symbol --> used for finding the uniqueness.
+    undefines --> before initialization of variable
+    null --> standalone value 
+
+b. Reference Datatype:-
+    object. --> array, function and object everythings comes under reference type data type.
+*/
+
+/*
+Note:- typeof methos are used to find the data type of the passed variable/function.
+Typeof null will give result as "Object". and type of undefined give result as "undefined".
+*/
+
+function myFun(){
+    console.log("Ravi");
+}
+myFun();
+
+console.log(typeof myFun);
+console.log(typeof myFun());
+
+console.log(typeof null);
+console.log(typeof undefined);
