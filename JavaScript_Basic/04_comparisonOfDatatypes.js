207cbc96cecadd063ae6645b296ae11f27d4be97
Raviranjan15April <146761397+Raviranjan15April@users.noreply.github.com>
2024-01-07 12:33:56 +0000

    comparison of different data types

---
+++ JavaScript_Basic/04_comparisonOfDatatypes.js
@@ -0,0 +1,16 @@
+console.log(null > 0);              // false
+console.log(null < 0);              // false
+console.log(null == 0);             // false
+console.log(null === 0);            // false
+console.log(null >= 0);             // true
+
+console.log(undefined > 0);              // false
+console.log(undefined < 0);              // false
+console.log(undefined == 0);             // false
+console.log(undefined === 0);            // false
+console.log(undefined >= 0);             // false
+
+/* 
+Difference between double equal(==) & triple equal(===):-
+double equal(==) will check only value but triple equal(===) will check value with data type as well.
+*/
