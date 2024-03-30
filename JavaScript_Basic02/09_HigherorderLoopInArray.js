69968495bd221b2f2fbe2a6cd5f1ff3bdee9226a
Raviranjan15April <146761397+Raviranjan15April@users.noreply.github.com>
2024-03-30 12:36:08 +0000

    Higher Order Loop of Array in Javascript

--- JavaScript_Basic02/09_HigherorderLoopInArray.js
+++ JavaScript_Basic02/09_HigherorderLoopInArray.js
@@ -1,4 +1,4 @@
-// For of loop :- it will give you iterable.
+// For of loop :- it will give you value/iterable from the array. Basically for-of loop will be used in array 
 
 let arr = [ "john", "peter", "devid"];
 for(let val of arr){
@@ -36,6 +36,38 @@ let myObj01 ={
     job : "Softwere Engineer"
 }
 
-for(let obj of myObj01){
+// for(let obj of myObj01){
     // console.log(obj);        // thi will give ypu error that myObj01 is not iterable.
+// }
+
+// for-in loop:- this loop will return the index/key from the array/object. Basically this loop mostly will be used in object.
+let myObj02 ={
+    name : "John",
+    age: 28,
+    job : "Softwere Engineer"
 }
+
+// for-in loop with object
+for(let key in myObj02){
+    console.log(`key of Object ${key} and their respective value ${myObj02[key]}`);
+}
+
+// for-in loop with Array:- it will always return undefined.
+let arr01 = ["JavaScript", "Java", "Ruby"];
+for(let ele in arr01){
+    console.log(ele);       // Output:- it will give you index of array; yes index array is number 
+}
+
+// forEach Loop:-
+let arr02 = ["JavaScript", "Java", "Ruby", "python"];
+// forEach method always return undefined
+let result = arr02.forEach((ele)=>{
+    return ele;       
+});
+console.log(result);            // Output:- undefined
+
+arr02.forEach((ele)=>{
+    console.log( ele );         // Output:- JavaScript/Java/Ruby/python
+});
+
+// Note:- callback function in forEach loop take iterable. index and whole array as an parameter.
